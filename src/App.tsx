567594c1c6dd0a1f1f44df9c5d5b9acffbda3566
D1msn <flash.volkov@gmail.com>
2021-11-09 15:12:56 +0300

    fix icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import Counter from "./components/Counter/Counter";
 
 function App() {
-    const [minCount, setMinCount] = useState<number>(0)
-    const [maxCount, setMaxCount] = useState<number>(5)
-    const [increment, setIncrement] = useState<number>(1)
+	const [minCount, setMinCount] = useState<number>(0)
+	const [maxCount, setMaxCount] = useState<number>(5)
+	const [increment, setIncrement] = useState<number>(1)
 
-  return (
-    <div className="App">
-      <Counter minCount={minCount} maxCount={maxCount} incrementCount={increment} />
-        <div className="counter-setter">
-            <input value={minCount} onChange={(e) => setMinCount(+e.currentTarget.value)} type="number"/>
-            <input value={maxCount} onChange={(e) => setMaxCount(+e.currentTarget.value)} type="number"/>
-            <input value={increment} onChange={(e) => setIncrement(+e.currentTarget.value)} type="number"/>
-        </div>
-    </div>
-  );
+	const pushIncrement = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = +e.currentTarget.value
+		if (value < 1) {
+			setIncrement(1)
+		} else {
+			setIncrement(value)
+		}
+	}
+
+	return (
+		<div className="App">
+			<Counter minCount={minCount} maxCount={maxCount} incrementCount={increment}/>
+			<div className="counter-setter">
+				<div className="counter-setter__min">
+					<input value={minCount} onChange={(e) => setMinCount(+e.currentTarget.value)} type="number"/>
+					Минимальное значение
+				</div>
+				<div className="counter-setter__max">
+					<input value={maxCount} onChange={(e) => setMaxCount(+e.currentTarget.value)} type="number"/>
+					Максимальное значение
+				</div>
+				<div className="counter-setter__imcrement">
+					<input value={increment} onChange={pushIncrement} type="number"/>
+					Шаг
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
