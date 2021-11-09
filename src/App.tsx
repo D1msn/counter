16b0f1cce15051b84c1573fb71932def11fa441a
D1msn <flash.volkov@gmail.com>
2021-11-09 09:32:28 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Counter from "./components/Counter/Counter";
 
 function App() {
+    const [minCount, setMinCount] = useState<number>(0)
+    const [maxCount, setMaxCount] = useState<number>(5)
+    const [increment, setIncrement] = useState<number>(1)
+
   return (
     <div className="App">
       <Counter minCount={0} maxCount={5} incrementCount={1} />
+        <div className="counter-setter">
+            <input value={minCount} onChange={(e) => setMinCount(+e.currentTarget.value)} type="number"/>
+            <input value={maxCount} onChange={(e) => setMaxCount(+e.currentTarget.value)} type="number"/>
+            <input value={increment} onChange={(e) => setIncrement(+e.currentTarget.value)} type="number"/>
+        </div>
     </div>
   );
 }
